863114ed83f294c6d6018f9883d54238711a1e49
Thanveerul Aslam Ahamed <143065891+Thanveer-Aslam@users.noreply.github.com>
2026-03-03 22:42:39 +0530

    Fix Vercel SPA routing

--- client/src/api/donations.js
+++ client/src/api/donations.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`, // this line was inside api/donation.js
 });
 
 API.interceptors.request.use((req) => {
--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import API from "../api"; 
+import API from "../api/donations";
 
 
 const Auth = () => {
